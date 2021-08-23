acd53be5ec01e20bf119da7c5cc72fe17c71a166
Karthikrrao <50327987+Karthikrrao@users.noreply.github.com>
2021-08-23 15:34:13 +0530

    Update main.tf

--- demo1/main.tf
+++ demo1/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-07ffe7b5c785aaac1"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
