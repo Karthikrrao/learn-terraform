ab5fa5a62a6645eef5bc603e87e073a835f3b59d
Karthikrrao <50327987+Karthikrrao@users.noreply.github.com>
2021-08-24 09:29:49 +0530

    Update outputs.tf

--- demo1/outputs.tf
+++ demo1/outputs.tf
@@ -4,5 +4,5 @@ output "instance_id" {
 }
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = aws_instance.app_server.public_id
+  value = aws_instance.app_server.public_ip
 }
